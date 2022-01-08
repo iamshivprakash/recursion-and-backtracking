f86847e5e1494905eef3aa240d15100584ba2887
Shiv Prakash <76246308+iamshivprakash@users.noreply.github.com>
2022-01-09 03:35:16 +0530

    wrote a better solution

--- print_first_index.py
+++ print_first_index.py
@@ -7,7 +7,17 @@ def firstIndex(arr,index,data):
         return index
     return fiisa
     
+# Better Solution__________________
+def Fi(arr,index,data):
+    if index == len(arr):
+        return -1
+    if data == arr[index]:
+        return index
+    fiisa = Fi(arr,index+1,data)
+    return fiisa
+    
 arr = list(map(int,input().split()))
 x = int(input())
 print(firstIndex(arr,0,x))
+print(Fi(arr,0,x))
         
